512e3e37cf8f47c669596b975e3b970cd3cd93ca
Matt Eland <Matt.Eland@GMail.com>
2020-04-23 22:22:54 -0400

    Under couch object resolution and couch description.

--- src/World/LivingArea/UnderCouch.ts
+++ src/World/LivingArea/UnderCouch.ts
@@ -36,7 +36,7 @@ export class UnderCouch extends RoomBase {
 
   describe(context: CommandContext): void {
     context.addText(`This is my own personal den underneath the couch. Mommy doesn't like it when I go here, but it's dark and quiet ` +
-      `and a good place to rest before I'm ready to go back out to the living room.`);
+      `and a good place to rest before I'm ready to exit and go back out to the living room.`);
     context.addText(`It's hard to see anything in here. If something is here, I'll have to sniff for it.`);
   }
 }
